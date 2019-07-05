519a62c9e5fbacb9c8b10203d7e0d2a21e1e1729
Putiloff <reg@putiloff.com>
2019-07-05 21:09:57 +0300

    finished changeColor method

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 let app = new Vue({
   el: "#app",
-  data: () => ({})
+  data: () => ({
+    currentColor: ""
+  }),
+  methods: {
+    getRandomColor() {
+      this.currentColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+    }
+  },
+  created() {
+    this.getRandomColor();
+  }
 });
