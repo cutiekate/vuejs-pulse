8672c36db9306ee864df209967e8abde7bcd7515
Putiloff <reg@putiloff.com>
2019-07-05 22:09:56 +0300

    finished toggle button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
 let app = new Vue({
   el: "#app",
-  data: () => ({})
+  data: () => ({
+    isSuccess: false,
+    alertSettings: {
+      success: {
+        message: "Все круто!"
+      },
+      error: {
+        message: "Что-то пошло не так"
+      }
+    },
+    isShow: false
+  }),
+  computed: {
+    message: {
+      get() {
+        return this.isSuccess
+          ? this.alertSettings.success.message
+          : this.alertSettings.error.message;
+      },
+      set(val) {
+        this.alertSettings[this.isSuccess ? "success" : "error"].message = val;
+      }
+    }
+  },
+  methods: {
+    alert() {
+      this.isShow = !this.isShow;
+    },
+    checkAlertClass() {
+      return this.isSuccess ? "alert-success" : "alert-danger";
+    }
+  }
 });
