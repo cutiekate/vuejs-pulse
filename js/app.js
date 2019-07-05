c7cea0d36961152ec50f153520dfde95c0c26510
Putiloff <reg@putiloff.com>
2019-07-06 00:20:11 +0300

    get privatbank api / construct table and realized sort by click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
 let app = new Vue({
   el: "#app",
-  data: () => ({})
+  data: () => ({
+    api: null,
+    selected: "",
+    sortDirection: {
+      buy: "ASC",
+      sale: "ASC"
+    }
+  }),
+  methods: {
+    sortApi(field) {
+      this.sortDirection[field] =
+        this.sortDirection[field] === "ASC" ? "DESC" : "ASC";
+      this.api.sort((a, b) =>
+        this.sortDirection[field] === "ASC"
+          ? a[field] - b[field]
+          : b[field] - a[field]
+      );
+    }
+  },
+  created() {
+    axios
+      .get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+      .then(response => (this.api = response.data));
+  }
 });
